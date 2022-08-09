5279b2f7550bbf8f4d4fd790eecf0fecb9c443ae
ricardo gratereaux <91284152+RickyDEV1@users.noreply.github.com>
2022-08-08 20:19:19 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+const { response } = require('express');
+const { json } = require('body-parser');
+
+var app = express();
+var port = process.env.PORT || 3525;
+
+// Convierte una petición recibida (POST-GET...) a objeto JSON
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+let contacts = [];
+
+//pasamos los contactos a la variable contacts
+fetch('http://www.raydelto.org/agenda.php')
+.then(e => e.json())
+.then(content => contacts = content);
+
+
+app.get('/', function(req, res){
+	res.status(200).send({
+		message: 'GET Home route working fine!'
+	});
+});
+
+//guardar contacto
+app.post('/contacts', (req, res) => {
+    const contact = {
+        nombre: req.contact.nombre,
+        apellido: req.contact.apellido,
+        telefono: req.contact.telefono
+    }
+
+    contacts.push(contact)
+
+    const response = {
+        data: contact,
+        message: 'Contacto agregado correctamente'
+    }
+
+    res.send(response)
+})
+
+//listar contactos
+app.get('/contacts', (req, res) => {
+    const response = {
+        data: contacts
+    }
+
+    res.send(response)
+})
+
+//obtener el puerto
+app.listen(port, function(){
+	console.log(`Server running in http://localhost:${port}`);
+	console.log('Defined routes:');
+	console.log('	[GET] http://localhost:3525/');
+});
